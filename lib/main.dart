f49523ac14cbe21975488872cb66a7031e024bb8
Kamrul Hasan <113679107+Kamrul-Hasan-2@users.noreply.github.com>
2024-06-24 23:13:00 +0600

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,9 @@
-main(){
-  print('hello wwold');
+import 'package:kamrul_live_test_1/media.dart';
+
+void  main(){
+  Media media = Media();
+  media.play();
+
+  Song song = Song(artist: 'Kamrul Hasan');
+  song.play();
 }
---
+++ lib/media.dart
@@ -0,0 +1,15 @@
+class Media {
+  void play(){
+    print('Playing media..');
+  }
+}
+
+class Song extends Media{
+ late String artist;
+ Song({required this.artist});
+  
+  @override
+  void play() {
+    print('Playing song by $artist...');
+  }
+}
